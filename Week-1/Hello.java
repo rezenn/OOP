03430b43aa3a9e70ba64c20529ae7132ea63f5cb
rezen <rezen.khadgi@gmail.com>
2024-04-14 12:17:28 +0545

    Week 1 comment

--- Week-1/Hello.java
+++ Week-1/Hello.java
@@ -1,6 +1,6 @@
-
 public class  Hello{
     public static void main(String[] args) {
+        /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java. */
         System.out.println("Hello World!");
     }
 
